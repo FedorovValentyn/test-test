9c6e49334c75695a645de56c6218ff21bb30e083
valentyn <valikfed0003@gmail.com>
2025-05-08 04:16:21 +0300

    I hope its final commit

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -132,3 +132,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+PORT = os.environ.get('PORT', 8000)  # Для локального тестування
